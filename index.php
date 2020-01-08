31c2d51471f6cb820fc1b6891ae7c3277bf0a6cf
Baljinder Singh Baljinder Singh <bbaljind@isimtl.local>
2020-01-08 16:12:20 -0500

    first file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+   // phpinfo();
+   $page_title = 'Home';
+    require_once('views/page_top.php')
+?>
+
+<main>
+</main>
+
+<?php 
+   // phpinfo();
+    require_once('views/page_bottom.php')
+?>
---
+++ views/page_top.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $page_title ?></title>
+   
+</head>
+<body>
+  <!-- nav>ul>li*4>a -->
+    <header class="top">
+        <h1><?= $page_title ?></h1>
+        <nav >
+            <a href = "index.php">Home</a>
+            <a href = "catalog.php">Catalog</a>
+        </nav>
+    </header>
